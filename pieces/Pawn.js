c672ad5aade3ef4a27f6dcee84850397ce08dd1d
Adriano <adrianopchm@gmail.com>
2024-07-03 11:19:58 -0500

    Javascript-Chess-Game/Game.js

--- pieces/Pawn.js
+++ pieces/Pawn.js
@@ -13,6 +13,18 @@ class Pawn extends Piece {
 		}
 
 		const attackMoves = [position + mathSign * 9, position + mathSign * 11];
+
+		// En passant conditions
+		if ((this.color === 'white' && Math.floor(position / 10) === 5) ||
+			(this.color === 'black' && Math.floor(position / 10) === 4)) {
+			if (game.enPassantTarget === position + mathSign * 9) {
+				attackMoves.push(position + mathSign * 9);
+			}
+			if (game.enPassantTarget === position + mathSign * 11) {
+				attackMoves.push(position + mathSign * 11);
+			}
+		}
+
 		return [attackMoves, allowedMoves];
 	}
 }
