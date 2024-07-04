3b69e5114df69c1c4f1bd1574e2880d862a2dae5
Adriano <adrianopchm@gmail.com>
2024-07-04 13:10:26 -0500

    Javascript-Chess-Game/pieces/Piece.js

--- pieces/Piece.js
+++ pieces/Piece.js
@@ -1,9 +1,9 @@
 class Piece {
 	constructor(position, rank, name) {
 		this.position = position;
-		this.rank     = rank;
-		this.name     = name;
-		this.color    = this.name.substring(0,5);
+		this.rank = rank;
+		this.name = name;
+		this.color = this.name.substring(0, 5);
 	}
 
 
@@ -15,40 +15,44 @@ class Piece {
 		this.position = parseInt(position);
 	}
 
+	getEnPassantMoves(lastMove) {
+		return [];
+	}
+
 	getAllowedMoves() {
 		return [];
 	}
 
 	getMovesTop() {
 		const movesTop = [];
-		for (let move = this.position+10; move <= 88; move+=10) movesTop.push(move);
+		for (let move = this.position + 10; move <= 88; move += 10) movesTop.push(move);
 		return movesTop;
 	}
 
 	getMovesBottom() {
 		const movesBottom = [];
-		for (let move = this.position-10; move >= 11 ; move-=10) movesBottom.push(move);
+		for (let move = this.position - 10; move >= 11; move -= 10) movesBottom.push(move);
 		return movesBottom;
 	}
 
 	getMovesRight() {
-		const num = this.position+'';
+		const num = this.position + '';
 		const movesRight = [];
-		for (let move = this.position+1; move <= parseInt(num[0]+'8'); move++) movesRight.push(move);
+		for (let move = this.position + 1; move <= parseInt(num[0] + '8'); move++) movesRight.push(move);
 		return movesRight;
 	}
 
 	getMovesLeft() {
-		const num = this.position+'';
+		const num = this.position + '';
 		const movesLeft = [];
-		for (let move = this.position-1; move >= parseInt(num[0]+'1'); move--) movesLeft.push(move);
+		for (let move = this.position - 1; move >= parseInt(num[0] + '1'); move--) movesLeft.push(move);
 		return movesLeft;
 	}
 
 	getMovesTopRight() {
 		const movesTopRight = [];
-		for (let move = this.position+11; move <= 88; move+=11) {
-			const firstDigit = (''+move)[1];
+		for (let move = this.position + 11; move <= 88; move += 11) {
+			const firstDigit = ('' + move)[1];
 			if (firstDigit > 8 || firstDigit < 1) break;
 			movesTopRight.push(move);
 		}
@@ -57,8 +61,8 @@ class Piece {
 
 	getMovesTopLeft() {
 		const movesTopLeft = [];
-		for (let move = this.position+9; move <= 88; move+=9) {
-			const firstDigit = (''+move)[1];
+		for (let move = this.position + 9; move <= 88; move += 9) {
+			const firstDigit = ('' + move)[1];
 			if (firstDigit > 8 || firstDigit < 1) break;
 			movesTopLeft.push(move);
 		}
@@ -67,8 +71,8 @@ class Piece {
 
 	getMovesBottomRight() {
 		const movesBottomRight = [];
-		for (let move = this.position-9; move >= 11 ; move-=9) {
-			const firstDigit = (''+move)[1];
+		for (let move = this.position - 9; move >= 11; move -= 9) {
+			const firstDigit = ('' + move)[1];
 			if (firstDigit > 8 || firstDigit < 1) break;
 			movesBottomRight.push(move);
 		}
@@ -77,8 +81,8 @@ class Piece {
 
 	getMovesBottomLeft() {
 		const movesBottomLeft = [];
-		for (let move = this.position-11; move >= 11 ; move-=11) {
-			const firstDigit = (''+move)[1];
+		for (let move = this.position - 11; move >= 11; move -= 11) {
+			const firstDigit = ('' + move)[1];
 			if (firstDigit > 8 || firstDigit < 1) break;
 			movesBottomLeft.push(move);
 		}
